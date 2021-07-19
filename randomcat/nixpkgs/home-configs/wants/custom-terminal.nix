7a3e24e3b9511adc4a863c49622e90c7e54ec657
Jason Cobb <jason.e.cobb@gmail.com>
2021-07-19 13:54:23 -0400

    Ensure that vim is set as EDITOR

--- randomcat/nixpkgs/home-configs/wants/custom-terminal.nix
+++ randomcat/nixpkgs/home-configs/wants/custom-terminal.nix
@@ -10,5 +10,18 @@
     programs.vim = {
       enable = true;
     };
+
+    programs.bash = {
+      enable = true;
+
+      initExtra = ''
+        . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+      '';
+    };
+
+    home.sessionVariables = {
+      EDITOR = config.home.sessionVariables.VISUAL;
+      VISUAL = "vim";
+    };
   };
 }
