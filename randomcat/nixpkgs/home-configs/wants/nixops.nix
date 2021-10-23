30c81f6ce0054a516fb732b1c5b512c12e23c980
Jason Cobb <jason.e.cobb@gmail.com>
2021-10-23 17:52:38 -0400

    Pin nixops because channel version does not work

--- randomcat/nixpkgs/home-configs/wants/nixops.nix
+++ randomcat/nixpkgs/home-configs/wants/nixops.nix
@@ -8,6 +8,15 @@
   };
 
   config = {
-    home.packages = [ pkgs.nixops ];
+    home.packages =
+      let
+        pinnedPkgs = import (builtins.fetchTarball {
+          url = "https://github.com/nixos/nixpkgs/archive/65b70fbe4c3a942a266794e28a08147b06ebb6bc.tar.gz";
+          sha256 = "0p2kprsxjvrn1m90h91dmjpgxxbbpa9xpwcz5zs2qzcaz279gdkm";
+        }) {};
+      in
+      [
+        pinnedPkgs.nixops
+      ];
   };
 }
