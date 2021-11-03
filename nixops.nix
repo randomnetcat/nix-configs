2aa07fb2d95a60dcf6c84f9e0db8e7e8403cfe1e
Jason Cobb <jason.e.cobb@gmail.com>
2021-11-03 00:18:30 -0400

    Add zulip server on oracle-server

--- nixops.nix
+++ nixops.nix
@@ -89,11 +89,30 @@
         (modulesPath + "/profiles/qemu-guest.nix")
         ./modules/system-types/oracle-arch.nix
         ./modules/wants/ssh-access.nix
+        ./zulip/module.nix
       ];
 
       boot.cleanTmpDir = true;
       networking.hostName = "instance-20211029-1400";
       networking.firewall.allowPing = true;
       services.openssh.enable = true;
+
+      # Have to use external nameserver because Oracle nameserver is on a link-local address (169.254.x.x) and docker adds routes for those addresses for its network adapters.
+      networking.nameservers = [ "1.1.1.1" ];
+
+      virtualisation.docker.enable = true;
+
+      services.randomcat.docker-zulip = {
+        enable = true;
+
+        secrets = {
+          memcachedPass = builtins.readFile ./secrets/zulip/memcached_pw;
+          rabbitMqPass = builtins.readFile ./secrets/zulip/rabbitmq_pw;
+          postgresPass = builtins.readFile ./secrets/zulip/postgres_pw;
+          redisPass = builtins.readFile ./secrets/zulip/redis_pw;
+          zulipSecretKey = builtins.readFile ./secrets/zulip/secret_key;
+          emailPassword = builtins.readFile ./secrets/zulip/sendgrid_api_key;
+        };
+      };
     };
  }
