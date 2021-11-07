bed6032f6a01565315c595a33611a665ea32b436
Jason Cobb <jason.e.cobb@gmail.com>
2021-11-06 23:21:06 -0400

    Move agorabot server to oracle-server

--- nixops.nix
+++ nixops.nix
@@ -9,20 +9,55 @@
         ./modules/system-types/basic-ovh.nix
         ./modules/wants/ssh-access.nix
         ./modules/wants/local-root-access.nix
-        ./modules/wants/agorabot.nix
-        ./modules/wants/agorabot-server.nix
       ];
-
-      services.randomcat.agorabot-server = {
-        enable = true;
-        user = "discord-bot";
-      };
 
       nix.gc.automatic = true;
       nix.gc.options = "--delete-older-than 30d";
       nix.optimise.automatic = true;
 
       networking.firewall.enable = false;
+   };
+
+    oracle-server = 
+    { config, pkgs, modulesPath, ... }:
+    {
+      deployment.targetHost = "158.101.116.179";
+
+      imports = [
+        (modulesPath + "/profiles/qemu-guest.nix")
+        ./modules/system-types/oracle-arch.nix
+        ./modules/wants/ssh-access.nix
+        ./modules/wants/zulip-server.nix
+        ./modules/wants/agorabot-server.nix
+      ];
+
+      boot.cleanTmpDir = true;
+      networking.hostName = "instance-20211029-1400";
+      networking.firewall.allowPing = true;
+      services.openssh.enable = true;
+
+      # Have to use external nameserver because Oracle nameserver is on a link-local address (169.254.x.x) and docker adds routes for those addresses for its network adapters.
+      networking.nameservers = [ "1.1.1.1" ];
+
+      virtualisation.docker.enable = true;
+
+      services.randomcat.docker-zulip = {
+        enable = true;
+
+        secrets = {
+          memcachedPass = builtins.readFile ./secrets/zulip/memcached_pw;
+          rabbitMqPass = builtins.readFile ./secrets/zulip/rabbitmq_pw;
+          postgresPass = builtins.readFile ./secrets/zulip/postgres_pw;
+          redisPass = builtins.readFile ./secrets/zulip/redis_pw;
+          zulipSecretKey = builtins.readFile ./secrets/zulip/secret_key;
+          emailPassword = builtins.readFile ./secrets/zulip/sendgrid_api_key;
+        };
+      };
+
+      services.randomcat.agorabot-server = {
+        enable = true;
+        user = "discord-bot";
+      };
 
       users.users.discord-bot = {
         group = "discord-bot";
@@ -78,41 +113,5 @@
           dataVersion = 1;
         };
       };
-    };
-
-    oracle-server = 
-    { config, pkgs, modulesPath, ... }:
-    {
-      deployment.targetHost = "158.101.116.179";
-
-      imports = [
-        (modulesPath + "/profiles/qemu-guest.nix")
-        ./modules/system-types/oracle-arch.nix
-        ./modules/wants/ssh-access.nix
-        ./modules/wants/zulip-server.nix
-      ];
-
-      boot.cleanTmpDir = true;
-      networking.hostName = "instance-20211029-1400";
-      networking.firewall.allowPing = true;
-      services.openssh.enable = true;
-
-      # Have to use external nameserver because Oracle nameserver is on a link-local address (169.254.x.x) and docker adds routes for those addresses for its network adapters.
-      networking.nameservers = [ "1.1.1.1" ];
-
-      virtualisation.docker.enable = true;
-
-      services.randomcat.docker-zulip = {
-        enable = true;
-
-        secrets = {
-          memcachedPass = builtins.readFile ./secrets/zulip/memcached_pw;
-          rabbitMqPass = builtins.readFile ./secrets/zulip/rabbitmq_pw;
-          postgresPass = builtins.readFile ./secrets/zulip/postgres_pw;
-          redisPass = builtins.readFile ./secrets/zulip/redis_pw;
-          zulipSecretKey = builtins.readFile ./secrets/zulip/secret_key;
-          emailPassword = builtins.readFile ./secrets/zulip/sendgrid_api_key;
-        };
-      };
-    };
+     };
  }
