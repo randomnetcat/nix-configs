16b7eabb9a4581d94a674e89e4e56130ae06633a
Jason Cobb <jason.e.cobb@gmail.com>
2021-11-07 15:42:22 -0500

    Add home-manager input

--- nixos_configuration/flake.nix
+++ nixos_configuration/flake.nix
@@ -1,5 +1,12 @@
 {
-  outputs = { self, nixpkgs }: {
+  inputs = {
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }: {
     nixosConfigurations.randomcat-laptop-nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ (import ./configuration.nix { deviceDir = ./modules/devices/dell-g5-laptop; }) ];
