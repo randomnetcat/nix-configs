0a98084b89aaa4b57ab1394b9e478d5633546a50
Jason Cobb <jason.e.cobb@gmail.com>
2021-12-04 22:06:00 -0500

    Bump AgoraBot

--- nixops.nix
+++ nixops.nix
@@ -21,6 +21,19 @@
 
       virtualisation.docker.enable = true;
 
+      users.users.remote-build = {
+        isNormalUser = true;
+        group = "remote-build";
+
+        openssh.authorizedKeys.keys = [
+          "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC06hArkl5doDuQeOGemytTzpSHMrCIYWae9/LfYRlxMR4EsX/UAELR+s/+su5RZjsKKgFlw5kAI+RvrtV9PkToOTEZCc1qZR4PDCv1aMUCUaxlMgEwNel8Wtt8IcbURCBPa28jZKnrJ24ZhsweXLgY1ZzPO2Uza1ZMOgALLSs7xOKd13nqehg7wOjX+zJAaJL73DTi8leeUsONIjGqsuPn6/yy7iH+chgYbb1ssjLNfsLVFih+fklL5vKv6sWbZud7dbQV2FVic1Kqc0pVL6xQVXL9hJo8WBgz/FTF933jRHWawOWDZYIl2OM3zt+jOHcJ3PM0pVXMktLXrDxqRJidYytNDhAfntooM5LQ612LGhRmHBvL1z5Qau14AhnEOqaLYtnMNif0ivKyNVaLYo8MrV5DoFjYcz8g4mOuM++JZ9Uo2MLAiGGWGdwo7bYWEmRJ6o36ZptE5e8Kvc4BsSwnI5axEU4HWGdHELT+sLqrt7Za5p87Z3mvkzxBTeRFKvM= root@randomcat-laptop-nixos"
+        ];
+      };
+
+      users.groups.remote-build = {};
+
+      nix.trustedUsers = [ "remote-build" ];
+
       randomcat.secrets = {
         sshPubKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOccenq6rA3lk3UtC0ywkJiiNV+76o6RQsfIQMY8cLw5 root@instance-20211029-1400";
 
@@ -37,7 +50,7 @@
           package = import (builtins.fetchGit {
             url = "https://github.com/randomnetcat/AgoraBot.git";
             ref = "main";
-            rev = "40a37f69da66a4cc92362cb05f12af12722c7772";
+            rev = "0b6f891c859f579675e453da477ad804f0ac1fcd";
           }) { inherit pkgs; };
 
           token = builtins.readFile ./secrets/discord/agora-prod-token;
@@ -69,7 +82,7 @@
           package = import (builtins.fetchGit {
             url = "https://github.com/randomnetcat/AgoraBot.git";
             ref = "secret-hitler";
-            rev = "77843d89a3f45b5e3f1dbffc6efc397ea6f9281d";
+            rev = "40fe5f0d2a98582b38fe07cba605376857f0c14c";
           }) { inherit pkgs; };
 
           token = builtins.readFile ./secrets/discord/secret-hitler-token;
