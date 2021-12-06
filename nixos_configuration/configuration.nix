85d21b83bf9651aadb4a2b636742fb7af16ab9fa
Jason Cobb <jason.e.cobb@gmail.com>
2021-12-05 22:16:04 -0500

    Remove external-display specialization

--- nixos_configuration/configuration.nix
+++ nixos_configuration/configuration.nix
@@ -28,12 +28,4 @@ in
     source = (import selfLinkSpecPath).etc_nixos_dir;
   };
 
-  specialisation = {
-    external-display.configuration = {
-      system.nixos.tags = [ "external-display" ];
-      hardware.nvidia.prime.offload.enable = lib.mkForce false;
-      hardware.nvidia.powerManagement.enable = lib.mkForce false;
-    };
-  };
-
 }
