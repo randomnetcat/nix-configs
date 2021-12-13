dd32ec189ad41346bca4744628b4a4ff13be9850
Jason Cobb <jason.e.cobb@gmail.com>
2021-12-12 19:40:21 -0500

    Use built-in home-manager config for java

--- nixos_configuration/modules/impl/home/home-configs/wants/java-development.nix
+++ nixos_configuration/modules/impl/home/home-configs/wants/java-development.nix
@@ -25,9 +25,11 @@
       };
     };
 
+    programs.java.enable = true;
+    programs.java.package = pkgs.jdk;
+
     home.packages = [
       pkgs.jetbrains.idea-ultimate
-      pkgs.jdk
       pkgs.gradle
     ];
   };
