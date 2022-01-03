8d34613ccff012baf8d4edf9d8811e950af0f90c
Jason Cobb <jason.e.cobb@gmail.com>
2022-01-03 13:38:26 -0500

    Add nvidia-offload script

--- nixos_configuration/hosts/finch/graphical.nix
+++ nixos_configuration/hosts/finch/graphical.nix
@@ -27,6 +27,16 @@
 
     services.xserver.displayManager.sessionCommands = ''
       ${lib.getBin pkgs.xorg.xrandr}/bin/xrandr --setprovideroutputsource NVIDIA-G0 modesetting
-  '';
+    '';
+
+    environment.systemPackages = [
+      (pkgs.writeShellScriptBin "nvidia-offload" ''
+        export __NV_PRIME_RENDER_OFFLOAD=1
+        export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+        export __GLX_VENDOR_LIBRARY_NAME=nvidia
+        export __VK_LAYER_NV_optimus=NVIDIA_only
+        exec -a "$0" "$@"
+      '')
+    ];
   };
 }
