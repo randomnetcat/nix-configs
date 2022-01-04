5b2e1def452de2c1dfe2f10c90fc5ce9247eea28
Jason Cobb <jason.e.cobb@gmail.com>
2022-01-03 21:36:52 -0500

    Don't disable GDM Wayland

--- nixos_configuration/modules/impl/graphical/gnome-gdm.nix
+++ nixos_configuration/modules/impl/graphical/gnome-gdm.nix
@@ -11,7 +11,6 @@
    # Enable the X11 windowing system.
     services.xserver.enable = true;
     services.xserver.displayManager.gdm.enable = true;
-    services.xserver.displayManager.gdm.wayland = false;
     services.xserver.desktopManager.gnome.enable = true;
   };
 }
