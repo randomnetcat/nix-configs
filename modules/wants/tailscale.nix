039d1024c89212eebf817cd5f962d7dc1c89cbf8
Jason Cobb <jason.e.cobb@gmail.com>
2022-03-19 00:51:50 -0400

    Don't bail early for tailscale autoconnect

--- modules/wants/tailscale.nix
+++ modules/wants/tailscale.nix
@@ -33,12 +33,6 @@
         # wait for tailscaled to settle
         sleep 2
 
-        # check if we are already authenticated to tailscale
-        status="$(${pkgs.tailscale}/bin/tailscale status -json | ${pkgs.jq}/bin/jq -r .BackendState)"
-        if [ $status = "Running" ]; then # if so, then do nothing
-          exit 0
-        fi
-
         # otherwise authenticate with tailscale
         ${pkgs.tailscale}/bin/tailscale up -authkey "$(cat -- ${lib.escapeShellArg secretKeyPath})"
       '';
