87242a81b46636af08feeae9a2d184d43b330f32
Jason Cobb <jason.e.cobb@gmail.com>
2022-03-29 10:45:53 -0400

    Enable fwupd on finch

--- hosts/finch/default.nix
+++ hosts/finch/default.nix
@@ -44,6 +44,8 @@
 
     networking.hostName = "finch";
 
+    services.fwupd.enable = true;
+
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
     # on your system were taken. It‘s perfectly fine and recommended to leave
