817d631b24f7ca7d9381256d0c1891928b01a1a9
Jason Cobb <jason.e.cobb@gmail.com>
2022-03-28 23:46:07 -0400

    Use builtin home-manager files option for jdks dir

--- modules/impl/users/randomcat/home/home-configs/detail/jdks-dir.nix
+++ modules/impl/users/randomcat/home/home-configs/detail/jdks-dir.nix
@@ -27,34 +27,11 @@ in
     };
   };
 
-  config = {
-    home.file."dev/jdks/.keep" = lib.mkIf cfg.enable {
-      text = "";
-    };
-
-    home.activation =
-    let
-      jdkGcRootDir = "/nix/var/nix/gcroots/per-user/$USER/linked_jdks";
-      dag = lib.hm.dag;
-    in
-    {
-      makeClearJdkGcRootDir = dag.entryAfter ["writeBoundary"] ''
-        $DRY_RUN_CMD rm -rf $VERBOSE_ARG -- "${jdkGcRootDir}"
-        $DRY_RUN_CMD mkdir -p $VERBOSE_ARG -- "${jdkGcRootDir}"
-      '';
-
-      generateStaticJdks =
-        dag.entryAfter
-        ["writeBoundary" "makeClearJdkGcRootDir" "linkGeneration"]
-        (
-          let
-            linkJdkScript = name: jdk-pkg: ''
-              $DRY_RUN_CMD ln -fs $VERBOSE_ARG -T -- "${jdk-pkg}" "${jdkGcRootDir}/${name}"
-              $DRY_RUN_CMD ln -fs $VERBOSE_ARG -T -- "${jdk-pkg.home}" "$HOME/dev/jdks/${name}"
-            '';
-          in
-          lib.concatStrings (lib.mapAttrsToList (name: value: linkJdkScript name value.package) cfg.jdks)
-        );
-    };
+  config = lib.mkIf cfg.enable {
+    home.file = lib.mkMerge ([
+      {
+        "dev/jdks/.keep".text = "";
+      }
+    ] ++ (lib.mapAttrsToList (name: value: { "dev/jdks/${name}".source = "${value.package.home}"; }) cfg.jdks));
   };
 }
