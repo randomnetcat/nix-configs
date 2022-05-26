d3ea26a643cc8f86738aac3dfa279fc0f96a612e
Jason Cobb <jason.e.cobb@gmail.com>
2022-05-26 17:08:26 -0400

    [groves] Enable zfs autotrim

--- hosts/groves/filesystem.nix
+++ hosts/groves/filesystem.nix
@@ -3,8 +3,9 @@
 {
   config = {
     services.zfs.autoScrub.enable = true;
-    services.zfs.autoSnapshot.enable = true;
+    services.zfs.trim.enable = true;
 
+    services.zfs.autoSnapshot.enable = true;
     services.zfs.autoSnapshot.frequent = 12;
     services.zfs.autoSnapshot.daily = 14;
     services.zfs.autoSnapshot.weekly = 8;
