0fff24842dc345094b34ed5004f6c6141fed2475
Jason Cobb <jason.e.cobb@gmail.com>
2022-05-26 19:27:16 -0400

    [groves] Use nvidia drivers
    HDMI display works
    Steam games don't create weird effect where border is drawn a bunch of times after moving

--- hosts/groves/graphical.nix
+++ hosts/groves/graphical.nix
@@ -7,6 +7,27 @@
     ../../modules/impl/graphical/io/touchpad.nix
   ];
 
-  options = {
+  config = {
+    services.xserver.videoDrivers = [ "nvidia" ];
+    hardware.opengl.enable = true;
+
+    hardware.nvidia.prime = {
+      offload.enable = true;
+
+      nvidiaBusId = "PCI:1:0:0";
+      intelBusId = "PCI:0:2:0";
+    };
+
+    environment.systemPackages =
+      let
+        nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+          export __NV_PRIME_RENDER_OFFLOAD=1
+          export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+          export __GLX_VENDOR_LIBRARY_NAME=nvidia
+          export __VK_LAYER_NV_optimus=NVIDIA_only
+          exec -a "$0" "$@"
+        '';
+      in
+      [ nvidia-offload];
   };
 }
