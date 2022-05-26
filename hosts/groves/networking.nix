fbb7981cd0e2f0b25209892b174931774ee33e2b
Jason Cobb <jason.e.cobb@gmail.com>
2022-05-26 16:35:07 -0400

    [groves] Change name of ethernet interface
    Probably changed after adding SSD?

--- hosts/groves/networking.nix
+++ hosts/groves/networking.nix
@@ -12,7 +12,7 @@
     # Per-interface useDHCP will be mandatory in the future, so this generated config
     # replicates the default behaviour.
     networking.useDHCP = false;
-    networking.interfaces.enp45s0.useDHCP = true;
+    networking.interfaces.enp46s0.useDHCP = true;
     networking.interfaces.wlp0s20f3.useDHCP = true;
 
     environment.systemPackages = [
