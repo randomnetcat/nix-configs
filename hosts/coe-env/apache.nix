cf00600a292f9682875d25a2c100ba035e58af81
Jason Cobb <jason.e.cobb@gmail.com>
2022-07-17 20:23:19 -0400

    [coe-env] Disable PHP display_errors

--- hosts/coe-env/apache.nix
+++ hosts/coe-env/apache.nix
@@ -8,7 +8,7 @@
     services.httpd.phpPackage = pkgs.php81.buildEnv {
       extraConfig = ''
         include_path=".:/var/www/wolftech/_include"
-        display_errors=On
+        display_errors=Off
         log_errors=On
         error_log=/var/log/httpd/php_error.log
       '';
