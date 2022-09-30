b686cc2873805d40c5d6f7345cd7ed30d0d2628c
Jason Cobb <jason.e.cobb@gmail.com>
2022-09-30 01:39:56 -0400

    [groves] Enable more man pages

--- hosts/groves/development.nix
+++ hosts/groves/development.nix
@@ -15,7 +15,14 @@
       keep-derivations = true
     '';
 
-    environment.systemPackages = [ pkgs.man-pages pkgs.man-pages-posix ];
+    environment.systemPackages = [
+      pkgs.linux-manual
+      pkgs.man-pages
+      pkgs.man-pages-posix
+    ];
+
+    documentation.man.enable = true;
+    documentation.man.generateCaches = true;
     documentation.dev.enable = true;
   };
 }
