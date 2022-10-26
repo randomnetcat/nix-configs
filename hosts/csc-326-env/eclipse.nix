190cdaa5cf7370d2506ac492007bc8673de8eed3
Jason Cobb <jason.e.cobb@gmail.com>
2022-10-26 12:03:46 -0400

    [csc-326-env] Add java 17

--- hosts/csc-326-env/eclipse.nix
+++ hosts/csc-326-env/eclipse.nix
@@ -3,9 +3,10 @@
 {
   config = {
     environment.systemPackages = [
-      pkgs.eclipses.eclipse-jee
+      (pkgs.eclipses.override { jdk = pkgs.openjdk17; }).eclipse-jee
     ];
 
     environment.etc."jdks/11".source= pkgs.openjdk11.home;
+    environment.etc."jdks/17".source= pkgs.openjdk17.home;
   };
 }
