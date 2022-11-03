7e2178fa5b2e0064176d3c44cccfb9993e0df7c3
Jason Cobb <jason.e.cobb@gmail.com>
2022-11-02 21:36:20 -0400

    [groves] Fix sanoid use of sync snapshots

--- hosts/groves/backup.nix
+++ hosts/groves/backup.nix
@@ -13,7 +13,6 @@
       user = "root";
     };
 
-
     users.users.duplicati = {
       isSystemUser = true;
       uid = config.ids.uids.duplicati;
@@ -25,13 +24,15 @@
     services.syncoid = {
       enable = true;
 
-      commonArgs = [ "--no-privilege-elevation" "--no-sync-snap" "--keep-sync-snap" "--no-rollback" ];
+      commonArgs = [ "--no-privilege-elevation" "--no-rollback" ];
+      localSourceAllow = [ "bookmark" "hold" "send" "snapshot" "destroy" "mount" ];
 
       commands."zfs-rent-user" = {
         target = "sync-groves@randomcat.zfs.rent:nas_1758665d/safe/rpool_fxooop_bak/groves/user";
         source = "rpool_fxooop/groves/user";
         recursive = true;
         sshKey = "/var/lib/syncoid/id_ed25519_zfs_rent";
+        extraArgs = [ "--identifier=zfs-rent" ];
       };
 
       commands."zfs-rent-system" = {
@@ -39,6 +40,7 @@
         source = "rpool_fxooop/groves/system";
         recursive = true;
         sshKey = "/var/lib/syncoid/id_ed25519_zfs_rent";
+        extraArgs = [ "--identifier=zfs-rent" ];
       };
     };
   };
