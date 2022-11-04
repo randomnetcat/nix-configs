29675c432c829e7f0679bfa44d21ab4f033b1c0d
Jason Cobb <jason.e.cobb@gmail.com>
2022-11-04 00:55:19 -0400

    [reese] Enable syncoid sync snapshots

--- hosts/reese/backup.nix
+++ hosts/reese/backup.nix
@@ -11,13 +11,15 @@
     services.syncoid = {
       enable = true;
 
-      commonArgs = [ "--no-privilege-elevation" "--no-sync-snap" "--keep-sync-snap" "--no-rollback" ];
+      commonArgs = [ "--no-privilege-elevation" "--no-rollback" ];
+      localSourceAllow = [ "bookmark" "hold" "send" "snapshot" "destroy" "mount" ];
 
       commands."zfs-rent-system" = {
         target = "sync-reese@randomcat.zfs.rent:nas_1758665d/safe/rpool_sggau1_bak/reese/system";
         source = "rpool_sggau1/reese/system";
         recursive = true;
         sshKey = "/var/lib/syncoid/syncoid-id-zfs-rent";
+        extraArgs = [ "--identifier=zfs-rent" ];
       };
     };
 
