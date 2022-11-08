0dec52b4d2f38e5205ceb2cd6b0969df83a047e7
Jason Cobb <jason.e.cobb@gmail.com>
2022-11-07 20:15:46 -0500

    [reese] Fix nomic-space container networking

--- hosts/reese/mastodon/nomic-space.nix
+++ hosts/reese/mastodon/nomic-space.nix
@@ -8,6 +8,7 @@
   config = {
     randomcat.mastodon-containers.instances.nomic-space = {
       enable = true;
+      containerName = "mdn-nomic"; # this needs to be no longer than 13 characters apparently
       webDomain = "mastodon.nomic.space";
       localDomain = "nomic.space";
     };
