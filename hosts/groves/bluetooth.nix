5214245cc7e9a515a48cbe91e739f988d128729f
Jason Cobb <jason.e.cobb@gmail.com>
2022-12-11 15:49:57 -0500

    [sys] Switch to pipewire for audio

--- hosts/groves/bluetooth.nix
+++ hosts/groves/bluetooth.nix
@@ -2,18 +2,7 @@
 
 {
   config = {
-    # This makes blutetooth audio work
-    # From https://nixos.wiki/wiki/Bluetooth
-
-    hardware.pulseaudio.enable = true;
-
-    hardware.pulseaudio = {
-      package = pkgs.pulseaudioFull;
-    };
-
     hardware.bluetooth.enable = true;
-
-    nixpkgs.config.pulseaudio = true;
 
     hardware.bluetooth.settings = {
       General = {
--- sys/impl/graphical/common.nix
+++ sys/impl/graphical/common.nix
@@ -11,8 +11,15 @@
     # Enable touchpad support (enabled default in most desktopManager).
     services.xserver.libinput.enable = true;
 
-    # Enable sound
-    sound.enable = true;
-    hardware.pulseaudio.enable = true;
+    security.rtkit.enable = true;
+
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+
+    hardware.pulseaudio.enable = false;
   };
 }
