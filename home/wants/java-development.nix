df67b0f62e30bf6b691c10faa5bfe7fca02153bf
Jason Cobb <jason.e.cobb@gmail.com>
2022-12-11 15:50:10 -0500

    [home] Add pinned JDK 17 directory

--- home/wants/java-development.nix
+++ home/wants/java-development.nix
@@ -10,6 +10,7 @@
 
   config = {
     home.file."dev/jdks/current".source = pkgs.jdk.home;
+    home.file."dev/jdks/17".source = pkgs.jdk17.home;
     home.file."dev/jdks/11".source = pkgs.jdk11.home;
 
     programs.java.enable = true;
