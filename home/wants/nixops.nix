953692afeb83598daa2aa21b45f0cf51fdf21f13
Jason Cobb <jason.e.cobb@gmail.com>
2022-12-28 01:40:41 -0500

    [home] Allow certifi version wrongfully marked insecure

--- home/wants/nixops.nix
+++ home/wants/nixops.nix
@@ -9,5 +9,12 @@
 
   config = {
     home.packages = [ pkgs.nixopsUnstable ];
+
+    nixpkgs.config.permittedInsecurePackages = [
+      # nixpkgs thinks this is vulnerable to CVE-2022-23491, the inclusion of the
+      # TrustCor root certificates.
+      # nixpkgs is wrong, this version has the patch.
+      "python3.10-certifi-2022.12.7"
+    ];
   };
 }
