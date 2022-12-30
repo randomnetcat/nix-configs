093b2685325300b7f1d4f6a15db281e5c59122ae
Janet Cobb <jason.e.cobb@gmail.com>
2022-12-30 16:03:56 -0500

    [leon] Add stateVersion

--- hosts/leon/default.nix
+++ hosts/leon/default.nix
@@ -15,4 +15,6 @@
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDHagOaeTR+/7FL9sErciMw30cmV/VW8HU7J3ZFU5nj9 jason.e.cobb@gmail.com" 
   ];
+
+  system.stateVersion = "23.05";
 }
