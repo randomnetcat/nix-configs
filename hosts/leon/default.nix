2b620944a5fa9a6ccdeed0474b0f92b4a02bd6b0
Janet Cobb <jason.e.cobb@gmail.com>
2022-12-30 21:08:14 -0500

    [leon] Add existing sync users

--- hosts/leon/default.nix
+++ hosts/leon/default.nix
@@ -28,5 +28,23 @@
     ];
   };
 
+  users.users.sync-groves = {
+    uid = 1001;
+    isNormalUser = true;
+
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL3Cwz1mqIc0kiDDraAgyJK2GYY0pjl0U5g5fjR4KiyM groves syncoid for randomcat.zfs.rent"
+    ];
+  };
+
+  users.users.sync-reese = {
+    uid = 1002;
+    isNormalUser = true;
+
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICfo8KLuyakQ6J6qz8dUa4Y4xEt7aV3dD6ozk9jvvaTe reese syncoid key"
+    ];
+  };
+
   system.stateVersion = "23.05";
 }
