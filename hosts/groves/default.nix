ff13b203f7003b736e4dbcad05534c5067185a2c
Janet Cobb <jason.e.cobb@gmail.com>
2023-04-13 16:27:58 -0400

    [groves] Disable auto login
    Seems to put Gnome in a weird state (external display is in the wrong
    place, somtimes the machine just doesn't respond to input, etc.).

--- hosts/groves/default.nix
+++ hosts/groves/default.nix
@@ -46,8 +46,6 @@
 
     services.fwupd.enable = true;
 
-    services.xserver.displayManager.autoLogin.user = "randomcat";
-
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
     # on your system were taken. It‘s perfectly fine and recommended to leave
