ee60fd2af79bcbb0efce6c6c88e1d318323c0949
Janet Cobb <git@randomcat.org>
2023-06-03 17:45:57 -0400

    [coe-env] Don't display PHP errors in webpage

--- hosts/coe-env/apache.nix
+++ hosts/coe-env/apache.nix
@@ -8,7 +8,7 @@
     services.httpd.phpPackage = pkgs.php81.buildEnv {
       extraConfig = ''
         include_path=".:/var/www/_include"
-        display_errors=On
+        display_errors=Off
         log_errors=On
         error_log=/var/log/httpd/php_error.log
         upload_max_filesize=20M
