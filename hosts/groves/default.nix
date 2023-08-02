53b2751541939bbd76e0301608e0fb149b84e695
Janet Cobb <git@randomcat.org>
2023-08-02 11:00:33 -0400

    [groves] Temporarily allow non-root access to a USB SD Card reader

--- hosts/groves/default.nix
+++ hosts/groves/default.nix
@@ -45,6 +45,9 @@
 
     services.fwupd.enable = true;
 
+    # TEMPORARY: Allow non-root to access USB SD card reader for work
+    services.udev.extraRules = ''SUBSYSTEMS=="usb" ATTRS{idVendor}=="14cd" ATTRS{idProduct}=="1212" GROUP="wheel" ENV{UDISKS_AUTO}="0"'';
+
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
     # on your system were taken. It‘s perfectly fine and recommended to leave
