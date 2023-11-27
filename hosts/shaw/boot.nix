9a662e8916b4589b9212c90baf6ad98e827ec005
Janet Cobb <git@randomcat.org>
2023-11-26 21:08:48 -0500

    [shaw] Enable SSH in initrd

--- hosts/shaw/boot.nix
+++ hosts/shaw/boot.nix
@@ -10,5 +10,19 @@
 
     boot.loader.systemd-boot.editor = false;
     boot.loader.efi.efiSysMountPoint = "/boot";
+
+    boot.initrd.network.ssh = {
+      enable = true;
+      port = 33;
+
+      hostKeys = [
+        ./ssh/initrd_ssh_rsa_key
+        ./ssh/initrd_ssh_ed25519_key
+      ];
+
+      authorizedKeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDHagOaeTR+/7FL9sErciMw30cmV/VW8HU7J3ZFU5nj9 janet@randomcat.org"
+      ];
+    };
   };
 }
