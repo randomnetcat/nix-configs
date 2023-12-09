1bd61c515d36941afe54b5c2fd184c464f8ae50d
Janet Cobb <git@randomcat.org>
2023-12-09 02:05:24 -0500

    f module

--- hosts/shaw/boot.nix
+++ hosts/shaw/boot.nix
@@ -16,9 +16,37 @@
     #   emergencyAccess = "$y$j9T$VFWFsSdjfFxZ0ulNmde6z/$BGUb8vViS0moC3YLGdCF9Y4lrB697tO9AM3aFFoKpB3";
     # };
 
-    boot.kernelParams = [
-      "acpi_enforce_resources=lax"
-    ];
+    # Okay, this is *incredibly* stupid.
+    # 
+    # The kernel includes a builtin it87 module, but it's too old, and we can't
+    # use it. We can, however, use a modified version of the module maintained
+    # in a GitHub repo. nixpkgs includes a derivation that builds from that
+    # fork, but that's *also* too old, and we can't use it. So, use the latest
+    # commit of the fork (as of time of writing).
+    #
+    # For laziness, we just override the existing derivation. However, the
+    # nixpkgs it87 derivation builds the kernel module into a .ko file. This
+    # causes a problem when nixpkgs builds the final module tree (what becomes
+    # the kernel-modules directly). Although the extraModulePackages *should*
+    # take precedence over the in-tree modules, this is implemented (in
+    # pkgs.aggregateModules, used by system.ModulesTree) by just linking in the
+    # files from the extraModulePackages after. The problem occurs because the
+    # in-tree kernel packages are built to a compressed .ko.xz. So, when
+    # aggregateModules links our modified it87 derivation into the final module
+    # tree, it just links in the .ko file, leaving the .ko.xz file intact. And,
+    # apparently, modprobe prefers the .ko.xz file!
+    #
+    # So, we have a few options. One person
+    # (https://discourse.nixos.org/t/best-way-to-handle-boot-extramodulepackages-kernel-module-conflict/30729)
+    # disabled the in-tree it87 module, which does work. However, it modifies
+    # the kernel derivation, so if we did that we could no longer use the
+    # binary caches.
+    #
+    # Instead, we build the modified it87 module, then move the built result
+    # from the "kernel" directory to the "extra" directory. The "extra"
+    # directory is for out-of-tree modules, and it takes precedence over
+    # in-kernel modules.So, it will actually take precedence over the in-tree
+    # module, as it should.
 
     boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages.extend (self: super: {
       it87 = super.it87.overrideAttrs (old: rec {
@@ -35,7 +63,13 @@
     });
 
     boot.extraModulePackages = [
-      config.boot.kernelPackages.it87
+      (config.boot.kernelPackages.it87.overrideAttrs (old: {
+        fixupPhase = let kernelVersion = config.boot.kernelPackages.kernel.version; in ''
+          ${old.fixupPhase or ""}
+
+          mv -- ${lib.escapeShellArg "lib/modules/${kernelVersion}/kernel"} ${lib.escapeShellArg "lib/modules/${kernelVersion}/extra"}
+        '';
+      }))
     ];
   };
 }
