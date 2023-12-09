67b3eae2a64b46cc8793065a898ad1cf38a6af8c
Janet Cobb <git@randomcat.org>
2023-12-08 21:03:02 -0500

    initrd network

--- hosts/shaw/boot.nix
+++ hosts/shaw/boot.nix
@@ -11,18 +11,24 @@
     boot.loader.systemd-boot.editor = false;
     boot.loader.efi.efiSysMountPoint = "/boot";
 
-    boot.initrd.network.ssh = {
+    boot.initrd.network = {
       enable = true;
-      port = 33;
+      udhcpc.enable = true;
+      flushBeforeStage2 = true;
 
-      hostKeys = [
-        ./ssh/initrd_ssh_rsa_key
-        ./ssh/initrd_ssh_ed25519_key
-      ];
+      ssh = {
+        enable = true;
+        port = 33;
 
-      authorizedKeys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDHagOaeTR+/7FL9sErciMw30cmV/VW8HU7J3ZFU5nj9 janet@randomcat.org"
-      ];
+        hostKeys = [
+          ./ssh/initrd_ssh_rsa_key
+          ./ssh/initrd_ssh_ed25519_key
+        ];
+
+        authorizedKeys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDHagOaeTR+/7FL9sErciMw30cmV/VW8HU7J3ZFU5nj9 janet@randomcat.org"
+        ];
+      };
     };
   };
 }
