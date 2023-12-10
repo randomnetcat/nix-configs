b06b464131ab120e0bff308b6d40db424f7db91d
Janet Cobb <git@randomcat.org>
2023-12-10 02:34:50 -0500

    [sys] Make system.autoUpgrade.allowReboot a default value

--- sys/wants/auto-upgrade/auto-reboot.nix
+++ sys/wants/auto-upgrade/auto-reboot.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 
 {
   imports = [
@@ -8,7 +8,7 @@
   config = {
     system.autoUpgrade = {
       operation = "switch";
-      allowReboot = true;
+      allowReboot = lib.mkDefault true;
     };
   };
 }
