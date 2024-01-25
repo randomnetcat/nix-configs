8905b8e9f37c16644c63edc3aef89d9d5d40bacd
Janet Cobb <git@randomcat.org>
2024-01-25 15:39:54 -0500

    [groves] Automatically sync data to shaw

--- hosts/groves/backup.nix
+++ hosts/groves/backup.nix
@@ -55,6 +55,40 @@
 
       # Run every day at 16:00 UTC.
       interval = "*-*-* 16:00:00";
+
+      commands."shaw" = {
+        target = "sync-groves@shaw:nas_oabrke/data/backups/groves";
+        source = "rpool_fxooop/groves/safe";
+        recursive = true;
+        sshKey = "/var/lib/syncoid/id_ed25519_zfs_rent";
+      };
     };
+
+    systemd.timers =
+      let
+        commonConfig = {
+          Persistent = true;
+        };
+      in {
+        syncoid-shaw.timerConfig = commonConfig;
+      };
+
+    systemd.services =
+      let
+        commonConfig = {
+          unitConfig = {
+            StartLimitBurst = 3;
+            StartLimitIntervalSec = "12 hours";
+          };
+
+          serviceConfig = {
+            Restart = "on-failure";
+            RestartSec = "15min";
+            TimeoutStartSec = "2 hours";
+          };
+        };
+      in {
+        syncoid-shaw = commonConfig;
+      };
   };
 }
