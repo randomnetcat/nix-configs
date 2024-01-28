d3913f2766d9549fc8c133c40bf92b9e5924cddd
Janet Cobb <git@randomcat.org>
2024-01-28 14:01:10 -0500

    [sys] Remove virtualbox

--- sys/wants/virtualization.nix
+++ sys/wants/virtualization.nix
@@ -6,10 +6,5 @@
     programs.dconf.enable = true;
     environment.systemPackages = [ pkgs.virt-manager ];
     virtualisation.spiceUSBRedirection.enable = true;
-
-    virtualisation.virtualbox.host.enable = true;
-
-    # https://www.reddit.com/r/virtualbox/comments/v75l21/stuck_starting_virtual_box_20/
-    boot.kernelParams = [ "ibt=off" ];
   };
 }
