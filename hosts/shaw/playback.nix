9473fe7a5c6a728eb8e181e6841360c10e1de4da
Janet Cobb <git@randomcat.org>
2024-02-20 02:12:24 -0500

    [shaw] Allow hardware transcoding for Jellyfin

--- hosts/shaw/playback.nix
+++ hosts/shaw/playback.nix
@@ -7,6 +7,35 @@
       openFirewall = true;
     };
 
-    users.users.jellyfin.extraGroups = [ "archive" ];
+    users.users.jellyfin = {
+      extraGroups = [
+        "archive" # for file permissions
+        "render"  # for hardware transcoding
+      ];
+
+      packages = [
+        pkgs.jellyfin
+        pkgs.jellyfin-web
+        pkgs.jellyfin-ffmpeg
+      ];
+    };
+
+    # From https://nixos.wiki/wiki/Jellyfin
+
+    nixpkgs.config.packageOverrides = pkgs: {
+      vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+    };
+
+    hardware.opengl = {
+      enable = true;
+
+      extraPackages = [
+        pkgs.intel-media-driver
+        pkgs.vaapiIntel
+        pkgs.vaapiVdpau
+        pkgs.libvdpau-va-gl
+        pkgs.intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+      ];
+    };
   };
 }
