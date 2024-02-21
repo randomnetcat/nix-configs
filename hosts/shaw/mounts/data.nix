cf9aced0d301e8a24cb4c132b0b6842b46ccca39
Janet Cobb <git@randomcat.org>
2024-02-20 20:50:34 -0500

    [shaw] Automatically import data pool

--- hosts/shaw/mounts/data.nix
+++ hosts/shaw/mounts/data.nix
@@ -6,5 +6,21 @@
       luks-data0 /dev/disk/by-uuid/dffb688d-1028-486d-a86f-fe07cae741b5 /root/keys/luks-data
       luks-data1 /dev/disk/by-uuid/faedab93-742e-4e0c-a026-110e826a91e2 /root/keys/luks-data
     '';
+
+    boot.zfs.extraPools = [
+      "nas_oabrke"
+    ];
+
+    systemd.services."zfs-import-nas_oabrke" = {
+      wants = [
+        "systemd-cryptsetup@luks\\x2ddata0.service"
+        "systemd-cryptsetup@luks\\x2ddata1.service"
+      ];
+
+      after = [
+        "systemd-cryptsetup@luks\\x2ddata0.service"
+        "systemd-cryptsetup@luks\\x2ddata1.service"
+      ];
+    };
   };
 }
