ced2c88be39fd915cb97b1148991f0e5bdac811d
Janet Cobb <git@randomcat.org>
2024-04-08 13:04:17 -0400

    [home] Fix deploy-rs reference

--- home/wants/deployment.nix
+++ home/wants/deployment.nix
@@ -3,7 +3,7 @@
 {
   config = {
     home.packages = [
-      (pkgs.extend inputs.deploy-rs.overlay).deploy-rs.deploy-rs
+      (pkgs.extend inputs.deploy-rs.overlays.default).deploy-rs.deploy-rs
     ];
   };
 }
