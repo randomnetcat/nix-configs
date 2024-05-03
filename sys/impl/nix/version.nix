b88e248c739a94b52493647fd6a497480caa8d73
Janet Cobb <git@randomcat.org>
2024-05-03 17:03:44 -0400

    Use nixVersions.latest instead of (removed) nixVersions.unstable

--- sys/impl/nix/version.nix
+++ sys/impl/nix/version.nix
@@ -3,9 +3,7 @@
 {
   config = {
     nix = {
-      # 2.21.0 is broken apparently? https://github.com/NixOS/nix/issues/10238
-      # So, use 2.20 instead of 2.21.0. This will disable itself when any version after 2.21.0 is released.
-      package = if pkgs.nixVersions.unstable.version == "2.21.0" then pkgs.nixVersions.nix_2_20 else pkgs.nixVersions.unstable;
+      package = pkgs.nixVersions.latest;
 
       extraOptions = ''
         experimental-features = nix-command flakes
