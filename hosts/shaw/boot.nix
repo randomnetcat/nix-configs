f1580697810f91a1e8f0c3f02b4a5904b260827f
Janet Cobb <git@randomcat.org>
2024-05-03 17:23:49 -0400

    [shaw] Fix it87 hack

--- hosts/shaw/boot.nix
+++ hosts/shaw/boot.nix
@@ -27,21 +27,23 @@ in
     # For laziness, we just override the existing derivation. However, the
     # nixpkgs it87 derivation builds the kernel module into a .ko file. This
     # causes a problem when nixpkgs builds the final module tree (what becomes
-    # the kernel-modules directly). The fist problem occurs because the in-tree
-    # kernel packages are built to a compressed .ko.xz. So, when
+    # the kernel-modules directly). The first problem occurs because the
+    # in-tree kernel packages are built to a compressed .ko.xz. So, when
     # aggregateModules links our modified it87 derivation into the final module
     # tree, it just links in the .ko file, leaving the .ko.xz file intact. And,
     # apparently, modprobe prefers the .ko.xz file!
     #
     # Second, even if we fix this, although the extraModulePackages *should*
     # take precedence over the in-tree modules according to the wiki, it
-    # doesn't!  It instead gives an error about the conflicting file. (This is
+    # doesn't! It instead gives an error about the conflicting file. (This is
     # implemented (in pkgs.aggregateModules, used by system.ModulesTree.) So,
     # we can't do that either.
     #
-    # So, instead, we force modprobe to load our modified module instead. This
-    # should be fine, since the dependency information should hopefully match
-    # the built-in module.
+    # So, instead, we take the approach in [0]. modprobe will prefer kernel
+    # modules in the "updates" directory. So, we change the module derivation
+    # to build into that directory.
+    #
+    # [0]: https://github.com/NixOS/nixpkgs/pull/213773
 
     boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages.extend (self: super: {
       it87 = super.it87.overrideAttrs (old: rec {
@@ -54,26 +56,22 @@ in
           rev = "6392311da76b4868efd7b6db8101e10f9e453c75";
           sha256 = "bletkm02WDhhI9dXAPnxuS9uIr3rk4Af7GohwwSM+WQ=";
         };
+
+        makeFlags = (lib.filter (o: !(lib.hasPrefix "MODDESTDIR=" o)) old.makeFlags) ++ [
+          "MODDESTDIR=$(out)/lib/modules/${self.kernel.modDirVersion}/updates/drivers/hwmon"
+          "COMPRESS_XZ=y"
+        ];
       });
     });
+
+    boot.extraModulePackages = [
+      config.boot.kernelPackages.it87
+    ];
 
     boot.kernelModules = [ "it87" ];
 
     boot.extraModprobeConfig = ''
-      install it87 /run/current-system/sw/bin/modprobe --ignore-install ${it87Object} fix_pwm_polarity=1 $CMDLINE_OPTS
+      options it87 fix_pwm_polarity=1
     '';
-
-    system.checks = [
-      (pkgs.runCommand "check-it87-override" {} ''
-        MOD_PATH=${lib.escapeShellArg it87Object}
-
-        if [ -f "$MOD_PATH" ]; then
-          mkdir -- "$out"
-        else
-          echo "Path does not exist: $MOD_PATH"
-          exit 1
-        fi
-      '')
-    ];
   };
 }
