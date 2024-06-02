648c0070c46828f2cdae4f73bbad4c3b5ad04a21
Janet Cobb <git@randomcat.org>
2024-06-02 18:20:02 -0400

    [reese] Use fs-keys for MediaWiki

--- hosts/reese/nomic-web/wiki.nix
+++ hosts/reese/nomic-web/wiki.nix
@@ -13,162 +13,153 @@ in
 
     containers.wiki = {
       config = { config, lib, pkgs, ... }: {
-        system.stateVersion = "22.05";
+        imports = [
+          ../../../sys/impl/fs-keys.nix
+        ];
 
-        networking.useHostResolvConf = false;
-        networking.firewall.enable = false;
+        config = {
+          system.stateVersion = "22.05";
 
-        services.resolved.enable = true;
+          networking.useHostResolvConf = false;
+          networking.firewall.enable = false;
 
-        services.mediawiki = {
-          enable = true;
-          name = "Infinite Nomic Wiki";
+          services.resolved.enable = true;
 
-          httpd.virtualHost = {
-            hostName = wikiHost;
-            adminAddr = "admin@randomcat.org";
+          services.mediawiki = {
+            enable = true;
+            name = "Infinite Nomic Wiki";
 
-            forceSSL = false;
-            enableACME = false;
+            httpd.virtualHost = {
+              hostName = wikiHost;
+              adminAddr = "admin@randomcat.org";
 
-            listen = [
-              {
-                ip = containers.wiki.localIP6;
-                port = wikiPort;
-                ssl = false;
-              }
-            ];
-            
+              forceSSL = false;
+              enableACME = false;
+
+              listen = [
+                {
+                  ip = containers.wiki.localIP6;
+                  port = wikiPort;
+                  ssl = false;
+                }
+              ];
+              
+              extraConfig = ''
+                AllowEncodedSlashes NoDecode
+                RewriteEngine On
+                RewriteRule "^${wikiSubpath}/rest.php$" "/rest.php" [PT]
+                RewriteRule "^${wikiSubpath}/rest.php/(.*)$" "/rest.php/$1" [PT]
+              '';
+            };
+
+            passwordFile = "/run/keys/password-file";
+
+            extensions = {
+              # null -> use built-in plugin
+
+              CodeEditor = null;
+              CategoryTree = null;
+              Cite = null;
+              CiteThisPage = null;
+              ParserFunctions = null;
+              TextExtracts = null;
+              VisualEditor = null;
+              WikiEditor = null;
+
+              CodeMirror = pkgs.fetchzip {
+                url = "https://web.archive.org/web/20231227210041if_/https://extdist.wmflabs.org/dist/extensions/CodeMirror-REL1_41-f833cc4.tar.gz";
+                sha256 = "sha256-Rg6ua5trCDuCSh3jLzrUhIxZXEKiTo5htrUprNAAV68=";
+              };
+
+              MobileFrontend = pkgs.fetchzip {
+                url = "https://web.archive.org/web/20231227205620if_/https://extdist.wmflabs.org/dist/extensions/MobileFrontend-REL1_41-964152c.tar.gz";
+                sha256 = "sha256-o7SQCW6VB2OB1h2H1xR2FNZv7Q5tjkqr8EdLRZC+VR8=";
+              };
+
+              DarkMode = pkgs.fetchzip {
+                url = "https://web.archive.org/web/20231227205834if_/https://extdist.wmflabs.org/dist/extensions/DarkMode-REL1_41-0dfc34e.tar.gz";
+                sha256 = "sha256-AHI+PyA4ufz3SiaQo6v0Ud27imWHfxJYpkZWG0PaR0Y=";
+              };
+            };
+
+            skins = {
+              MinervaNeue = "${config.services.mediawiki.package}/share/mediawiki/skins/MinervaNeue";
+            };
+
             extraConfig = ''
-              AllowEncodedSlashes NoDecode
-              RewriteEngine On
-              RewriteRule "^${wikiSubpath}/rest.php$" "/rest.php" [PT]
-              RewriteRule "^${wikiSubpath}/rest.php/(.*)$" "/rest.php/$1" [PT]
+              # $wgDebugLogFile = '/var/log/mediawiki/debug.log';
+
+              $wgForceHTTPS = true;
+              $wgServer = 'https://${wikiHost}';
+              $wgInternalServer = 'http://[${containers.wiki.localIP6}]:${toString wikiPort}';
+
+              $wgCdnServersNoPurge = array();
+              $wgCdnServersNoPurge[] = '${containers.wiki.hostIP6}';
+              $wgUsePrivateIPs = true;
+              $wgScriptPath = '${wikiSubpath}';
+              $wgResourceBasePath = '${wikiSubpath}';
+              $wgRestPath = '${wikiSubpath}/rest.php';
+              $wgLogo = '${wikiSubpath}/images/logo';
+
+              $wgEmergencyContact = 'admin@randomcat.org';
+              $wgPasswordSender = 'wiki@unspecified.systems';
+
+              $wgSMTP = [
+                'host' => 'tls://mail.unspecified.systems',
+                'IDHost' => 'unspecified.systems',
+                'localhost' => 'unspecified.sytems',
+                'port' => 465,
+                'auth' => true,
+                'username' => 'wiki@unspecified.systems',
+                'password' => trim(file_get_contents('/run/keys/smtp-pass')),
+              ];
+
+              $wgAllowHTMLEmail = true;
+
+              // CodeEditor
+              $wgDefaultUserOptions['usebetatoolbar'] = 1; // user option provided by WikiEditor extension
+
+              // CodeMirror
+              $wgDefaultUserOptions['usecodemirror'] = 1;
+              $wgCodeMirrorEnableBracketMatching = true;
+              $wgCodeMirrorAccessibilityColors = true;
+              $wgCodeMirrorLineNumberingNamespaces = null;
+
+              // MobileFrontend
+              $wgDefaultMobileSkin = 'minerva';
+
+              // Parsoid (for VisualEditor)
+              $wgVirtualRestConfig['modules']['parsoid'] = [
+                'url' => $wgInternalServer . $wgRestPath,
+              ];
+
+              // From https://www.mediawiki.org/wiki/Parsoid
+              wfLoadExtension('Parsoid', "$IP/vendor/wikimedia/parsoid/extension.json");
+
+              // SVG
+              $wgSVGConverters = [
+                'batik' => '${pkgs.jdk}/bin/java -Djava.awt.headless=true -jar ${pkgs.batik}/batik-rasterizer-${pkgs.batik.version}.jar -w $width -d $output $input'
+              ];
+
+              $wgSVGConverter = "batik";
+
+              // External images
+              $wgAllowExternalImagesFrom = [
+                'https://nyhilo.website/cycle15/'
+              ];
             '';
           };
 
-          passwordFile = "/run/keys/password-file";
+          randomcat.services.fs-keys.mediawiki-creds = {
+            before = [ "mediawiki-init.service" ];
+            requiredBy = [ "mediawiki-init.service" ];
 
-          extensions = {
-            # null -> use built-in plugin
-
-            CodeEditor = null;
-            CategoryTree = null;
-            Cite = null;
-            CiteThisPage = null;
-            ParserFunctions = null;
-            TextExtracts = null;
-            VisualEditor = null;
-            WikiEditor = null;
-
-            CodeMirror = pkgs.fetchzip {
-              url = "https://web.archive.org/web/20231227210041if_/https://extdist.wmflabs.org/dist/extensions/CodeMirror-REL1_41-f833cc4.tar.gz";
-              sha256 = "sha256-Rg6ua5trCDuCSh3jLzrUhIxZXEKiTo5htrUprNAAV68=";
-            };
-
-            MobileFrontend = pkgs.fetchzip {
-              url = "https://web.archive.org/web/20231227205620if_/https://extdist.wmflabs.org/dist/extensions/MobileFrontend-REL1_41-964152c.tar.gz";
-              sha256 = "sha256-o7SQCW6VB2OB1h2H1xR2FNZv7Q5tjkqr8EdLRZC+VR8=";
-            };
-
-            DarkMode = pkgs.fetchzip {
-              url = "https://web.archive.org/web/20231227205834if_/https://extdist.wmflabs.org/dist/extensions/DarkMode-REL1_41-0dfc34e.tar.gz";
-              sha256 = "sha256-AHI+PyA4ufz3SiaQo6v0Ud27imWHfxJYpkZWG0PaR0Y=";
-            };
+            keys.smtp-pass.source.inherited = true;
+            keys.password-file.source.inherited = true;
           };
 
-          skins = {
-            MinervaNeue = "${config.services.mediawiki.package}/share/mediawiki/skins/MinervaNeue";
-          };
-
-          extraConfig = ''
-            # $wgDebugLogFile = '/var/log/mediawiki/debug.log';
-
-            $wgForceHTTPS = true;
-            $wgServer = 'https://${wikiHost}';
-            $wgInternalServer = 'http://[${containers.wiki.localIP6}]:${toString wikiPort}';
-
-            $wgCdnServersNoPurge = array();
-            $wgCdnServersNoPurge[] = '${containers.wiki.hostIP6}';
-            $wgUsePrivateIPs = true;
-            $wgScriptPath = '${wikiSubpath}';
-            $wgResourceBasePath = '${wikiSubpath}';
-            $wgRestPath = '${wikiSubpath}/rest.php';
-            $wgLogo = '${wikiSubpath}/images/logo';
-
-            $wgEmergencyContact = 'admin@randomcat.org';
-            $wgPasswordSender = 'wiki@unspecified.systems';
-
-            $wgSMTP = [
-              'host' => 'tls://mail.unspecified.systems',
-              'IDHost' => 'unspecified.systems',
-              'localhost' => 'unspecified.sytems',
-              'port' => 465,
-              'auth' => true,
-              'username' => 'wiki@unspecified.systems',
-              'password' => trim(file_get_contents('/run/keys/smtp-pass')),
-            ];
-
-            $wgAllowHTMLEmail = true;
-
-            // CodeEditor
-            $wgDefaultUserOptions['usebetatoolbar'] = 1; // user option provided by WikiEditor extension
-
-            // CodeMirror
-            $wgDefaultUserOptions['usecodemirror'] = 1;
-            $wgCodeMirrorEnableBracketMatching = true;
-            $wgCodeMirrorAccessibilityColors = true;
-            $wgCodeMirrorLineNumberingNamespaces = null;
-
-            // MobileFrontend
-            $wgDefaultMobileSkin = 'minerva';
-
-            // Parsoid (for VisualEditor)
-            $wgVirtualRestConfig['modules']['parsoid'] = [
-              'url' => $wgInternalServer . $wgRestPath,
-            ];
-
-            // From https://www.mediawiki.org/wiki/Parsoid
-            wfLoadExtension('Parsoid', "$IP/vendor/wikimedia/parsoid/extension.json");
-
-            // SVG
-            $wgSVGConverters = [
-              'batik' => '${pkgs.jdk}/bin/java -Djava.awt.headless=true -jar ${pkgs.batik}/batik-rasterizer-${pkgs.batik.version}.jar -w $width -d $output $input'
-            ];
-
-            $wgSVGConverter = "batik";
-
-            // External images
-            $wgAllowExternalImagesFrom = [
-              'https://nyhilo.website/cycle15/'
-            ];
-          '';
+          users.users.mediawiki.extraGroups = [ "keys" ];
         };
-
-        systemd.services.mediawiki-creds = {
-          before = [ "mediawiki-init.service" ];
-          requiredBy = [ "mediawiki-init.service" ];
-
-          script = ''
-            SECRET_DIR="$(mktemp -d)"
-            install -m 0750 -o root -g keys -T -- "$CREDENTIALS_DIRECTORY/smtp-pass" "$SECRET_DIR/smtp-pass"
-            install -m 0750 -o root -g keys -T -- "$CREDENTIALS_DIRECTORY/password-file" "$SECRET_DIR/password-file"
-            mv -T -- "$SECRET_DIR/smtp-pass" /run/keys/smtp-pass
-            mv -T -- "$SECRET_DIR/password-file" /run/keys/password-file
-            rmdir -- "$SECRET_DIR"
-          '';
-
-          serviceConfig = {
-            Type = "oneshot";
-            RemainAfterExit = true;
-            PrivateTmp = true;
-            LoadCredential = [ "smtp-pass" "password-file" ];
-          };
-
-          unitConfig.RequiresMountsFor = [ "/run/keys" ];
-        };
-
-        users.users.mediawiki.extraGroups = [ "keys" ];
       };
 
       ephemeral = false;
