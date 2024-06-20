3f528016b6c4f98e475382f9fa02b4a963cb914e
Janet Cobb <git@randomcat.org>
2024-06-19 23:30:36 -0400

    [shaw] Automatically prune syncoid sync snapshots

--- hosts/shaw/backup/dest.nix
+++ hosts/shaw/backup/dest.nix
@@ -105,6 +105,74 @@ in
         '') sourceHosts)}
       '';
     };
+
+    systemd.services."sync-prune-snapshots" = {
+      wantedBy = [ "multi-user.target" ];
+      startAt = "06:00";
+
+      serviceConfig = {
+        Type = "oneshot";        
+      };
+
+      script = ''
+        set -euo pipefail
+
+        prune_dataset_snaps() {
+            declare -r dataset="$1"
+            declare -r prefix="$2"
+
+            echo "Pruning dataset: $dataset" >&2
+
+            # It's okay if grep returns no matches, so need to check exit status.
+            #
+            # Also, ignore the last two snapshots to ensure that the most recent
+            # common ancestor is not accidentally destroyed.
+
+            ${zfsBin} list -t snapshot -Ho name -s createtxg -s creation -- "$dataset" \
+                | (grep -F "$dataset@$prefix" || test "$?" = 1) \
+                | head -n -2 \
+                | {
+                    while IFS="" read -r snapshot; do
+                        # Refuse to ever destroy a snapshot not matching the pattern.
+                        if [[ "$snapshot" != "$dataset@$prefix"* ]]; then
+                            echo "Refusing to destroy snapshot: $snapshot"
+
+                            # There's a bug here, completely exit.
+                            exit 1
+                        fi
+
+                        ${zfsBin} destroy -v -- "$snapshot"
+                    done
+                }
+        }
+
+        prune_recursive_snaps() {
+            declare -r parent="$1"
+            declare -r prefix="$2"
+
+            echo "Pruning recursively from: $parent" >&2
+
+            ${zfsBin} list -t filesystem -rHo name -- "$parent" | {
+                while IFS="" read -r dataset; do
+                    if [[ "$dataset" != "$dataset"* ]]; then
+                        echo "Refusing to prune dataset: $dataset"
+                        exit 1
+                    fi
+
+                    prune_dataset_snaps "$dataset" "$prefix"
+                done
+            }
+        }
+
+        ${lib.concatLines (lib.mapAttrsToList (host: hostCfg: ''
+          ${lib.escapeShellArgs [
+            "prune_recursive_snaps"
+            "${backupsDataset}/${host}"
+            "syncoid_${host}"
+          ]} || printf "Failed to prune dataset: %s\n" ${lib.escapeShellArg "${backupsDataset}/${host}"}
+        '') sourceHosts)}
+      '';
+    };
   }]
   ++ (lib.mapAttrsToList (host: hostCfg: {
     users.users."sync-${host}" = {
