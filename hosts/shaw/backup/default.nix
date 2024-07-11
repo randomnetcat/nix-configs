84d41b4f5ad6bf6d84901631d192fb556f0ea5e1
Janet Cobb <git@randomcat.org>
2024-07-10 22:58:46 -0400

    [shaw] Add groves sync target

--- hosts/shaw/backup/default.nix
+++ hosts/shaw/backup/default.nix
@@ -11,6 +11,7 @@
       parentDataset = "nas_oabrke/data/backups";
 
       acceptSources = {
+        groves = {};
         reese.sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFkOFn/HmrUFe3/I8JI4tsRRmTtsjmSjMYruVaxrzmoV root@reese";
       };
     };
