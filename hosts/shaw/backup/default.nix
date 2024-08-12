ef3c8fdbc7d17e715f1ac67a0aae520220da0215
Janet Cobb <git@randomcat.org>
2024-08-12 11:27:23 -0400

    [shaw] Restore sanoid for pruning backups

--- hosts/shaw/backup/default.nix
+++ hosts/shaw/backup/default.nix
@@ -9,5 +9,32 @@
 
   config = {
     randomcat.services.backups.fromNetwork = true;
+
+    services.sanoid = {
+      enable = true;
+      interval = "*:0/15";
+      extraArgs = [ "--verbose" "--debug" ];
+
+      templates."safe_backup" = {
+        yearly = 999999;
+        monthly = 999999;
+        weekly = 999999;
+        daily = 999999;
+        hourly = 0;
+
+        autosnap = false;
+        autoprune = true;
+      };
+
+      datasets."nas_oabrke/data/backups/groves" = {
+        useTemplate = [ "safe_backup" ];
+        recursive = "zfs";
+      };
+
+      datasets."nas_oabrke/data/backups/reese" = {
+        useTemplate = [ "safe_backup" ];
+        recursive = "zfs";
+      };
+    };
   };
 }
