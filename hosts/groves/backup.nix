ce5f81d9c51e071941a7a6e87dd6941900e74f6a
Janet Cobb <git@randomcat.org>
2024-10-06 01:45:54 -0400

    [groves] Re-enable sanoid

--- hosts/groves/backup.nix
+++ hosts/groves/backup.nix
@@ -7,6 +7,32 @@
   ];
 
   config = {
+    services.sanoid = {
+      enable = true;
+      interval = "*:0/15";
+      extraArgs = [ "--verbose" "--debug" ];
+
+      settings."template_safe" = {
+        weekly = 8;
+        frequently = 12;
+        frequent_period = 15;
+      };
+
+      templates."safe" = {
+        yearly = 2;
+        monthly = 24;
+        daily = 14;
+        hourly = 4;
+        autosnap = true;
+        autoprune = true;
+      };
+
+      datasets."rpool_fxooop/groves/safe" = {
+        useTemplate = [ "safe" ];
+        recursive = "zfs";
+      };
+    };
+
     randomcat.services.backups.source = {
       fromNetwork = true;
       encryptedSyncKey = ./secrets/sync-key;
