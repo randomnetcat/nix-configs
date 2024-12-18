d369edab51944de284085a21936e03b7f19d3489
Janet Cobb <git@randomcat.org>
2024-12-18 11:07:32 -0500

    [home] Re-enable colmena

--- home/wants/deployment.nix
+++ home/wants/deployment.nix
@@ -3,8 +3,7 @@
 {
   config = {
     home.packages = [
-      # Currently broken because of https://git.lix.systems/lix-project/nix-eval-jobs/pulls/14
-      # (pkgs.extend inputs.colmena.overlays.default).colmena
+      (pkgs.extend inputs.colmena.overlays.default).colmena
     ];
   };
 }
