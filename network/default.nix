3fab60f64fe6e69efed84a23ddafd6f5afa33ab2
Janet Cobb <git@randomcat.org>
2024-12-19 10:39:40 -0500

    [network] Update networks: hosts, keys, IPs

--- network/default.nix
+++ network/default.nix
@@ -9,12 +9,34 @@
   config = {
     randomcat.network = {
       hosts = {
-        shaw = {
-          hostKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMC0AomCZZiUV/BCpImiV4p/vGvFaz5QNc+fJLXmS5p root@shaw";
+        bear = {
+          hostKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIZ9Kn1CIcDHaleKHf7zO6O30Rbxs/FwL0/Ie+mEjZJr";
+          tailscaleIP4 = "100.85.165.130";
+          tailscaleIP6 = "fd7a:115c:a1e0:ab12:4843:cd96:6255:a582";
         };
 
-        reese = {};
-        groves = {};
+        groves = {
+          tailscaleIP4 = "100.68.110.33";
+          tailscaleIP6 = "fd7a:115c:a1e0:ab12:4843:cd96:6244:6e21";
+        };
+
+        leon = {
+          hostKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJCuMBdSMXPBTj00Z4oRcFuo/BEzbVogwYsvixhkBuU9";
+          tailscaleIP4 = "100.94.148.11";
+          tailscaleIP6 = "fd7a:115c:a1e0::d401:940b";
+        };
+
+        reese = {
+          hostKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPd0qGxvcMLDwX1bqYpwOUL5c/CIgBllMFr+bGkwiwAn";
+          tailscaleIP4 = "100.90.31.23";
+          tailscaleIP6 = "fd7a:115c:a1e0:ab12:4843:cd96:625a:1f17";
+        };
+
+        shaw = {
+          hostKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMC0AomCZZiUV/BCpImiV4p/vGvFaz5QNc+fJLXmS5p";
+          tailscaleIP4 = "100.103.37.71";
+          tailscaleIP6 = "fd7a:115c:a1e0::f567:2547";
+        };
       };
 
       backups = {
