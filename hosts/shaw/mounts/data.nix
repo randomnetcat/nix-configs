7c08ec2a45ac69f0879372e6afebee9a288b362e
Janet Cobb <git@randomcat.org>
2025-01-11 13:07:23 -0500

    shaw: update data device UUIDs for new drives

--- hosts/shaw/mounts/data.nix
+++ hosts/shaw/mounts/data.nix
@@ -3,8 +3,8 @@
 {
   config = {
     environment.etc."crypttab".text = ''
-      luks-data0 /dev/disk/by-uuid/dffb688d-1028-486d-a86f-fe07cae741b5 /root/keys/luks-data nofail,timeout=15
-      luks-data1 /dev/disk/by-uuid/faedab93-742e-4e0c-a026-110e826a91e2 /root/keys/luks-data nofail,timeout=15
+      luks-data0 /dev/disk/by-uuid/144cd1af-113a-40df-b19b-4de70e16f7db /root/keys/luks-data nofail,timeout=15
+      luks-data1 /dev/disk/by-uuid/4bfa4fb7-606d-493d-aaf8-c6f34ee4e6e8 /root/keys/luks-data nofail,timeout=15
       luks-data2 /dev/disk/by-uuid/b991dc4f-2bae-481c-8a77-20a140d99b00 /root/keys/luks-data nofail,timeout=15
       luks-data3 /dev/disk/by-uuid/d318e8f0-6aca-4115-bd9e-9840696e20f5 /root/keys/luks-data nofail,timeout=15
     '';
