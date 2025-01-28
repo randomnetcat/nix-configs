17fd55a7430f7a4fa806e178735a198f2350752b
Janet Cobb <git@randomcat.org>
2025-01-28 15:37:26 -0500

    reese: proxy cpp_initialization rather than redirecting

--- hosts/reese/sites/randomcat.org/default.nix
+++ hosts/reese/sites/randomcat.org/default.nix
@@ -9,6 +9,24 @@
       enableACME = true;
 
       locations."/".return = "307 https://randomnetcat.github.io$request_uri";
+
+      locations."= /cpp_initialization/initialization.svg" = {
+        proxyPass = "https://raw.githubusercontent.com/randomnetcat/cpp_initialization/gh-pages/initialization.svg";
+        recommendedProxySettings = false;
+
+        extraConfig = ''
+          proxy_set_header Host "raw.githubusercontent.com";
+        '';
+      };
+
+      locations."= /cpp_initialization/initialization.png" = {
+        proxyPass = "https://raw.githubusercontent.com/randomnetcat/cpp_initialization/gh-pages/initialization.png";
+        recommendedProxySettings = false;
+
+        extraConfig = ''
+          proxy_set_header Host "raw.githubusercontent.com";
+        '';
+      };
     };
   };
 }
