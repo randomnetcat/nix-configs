03bc19a630b76baa5affb6d2a1ee5d39facae951
Janet Cobb <git@randomcat.org>
2025-02-05 14:08:07 -0500

    network: namespace knownHosts attributes to avoid conflict with birdsong

--- presets/network.nix
+++ presets/network.nix
@@ -6,12 +6,19 @@
   ];
 
   config = {
-    programs.ssh.knownHosts = lib.concatMapAttrs
-      (_: host: {
-        "${host.hostName}".publicKey = host.hostKey;
-        "${host.tailscaleIP4}".publicKey = host.hostKey;
-        "${host.tailscaleIP6}".publicKey = host.hostKey;
-      })
-      (lib.filterAttrs (_: v: v.hostKey != null) config.randomcat.network.hosts);
+    programs.ssh.knownHosts =
+      lib.mkMerge (lib.mapAttrsToList
+        (name: host: {
+          "randomcat-${name}" = {
+            publicKey = host.hostKey;
+
+            hostNames = [
+              host.hostName
+              host.tailscaleIP4
+              host.tailscaleIP6
+            ];
+          };
+        })
+        (lib.filterAttrs (_: v: v.hostKey != null) config.randomcat.network.hosts));
   };
 }
