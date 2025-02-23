019ebb5c80ea47c12f27c065ea2215c1644ad624
Janet Cobb <git@randomcat.org>
2025-02-22 23:12:57 -0500

    carter: remove references to non-existent network interfaces

--- hosts/carter/networking.nix
+++ hosts/carter/networking.nix
@@ -10,7 +10,5 @@
   config = {
     # Let NetworkManager handle DHCP
     networking.useDHCP = false;
-    networking.interfaces.enp46s0.useDHCP = false;
-    networking.interfaces.wlp0s20f3.useDHCP = false;
   };
 }
