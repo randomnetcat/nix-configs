d6920c9fdf59c29c18cc56820b62062730d18cc8
Janet Cobb <git@randomcat.org>
2025-03-05 14:08:26 -0500

    home: add __pycache__ to global gitignore

--- home/detail/version-control-common.nix
+++ home/detail/version-control-common.nix
@@ -9,9 +9,23 @@
   config = {
     programs.git = {
       enable = true;
-      ignores = [ ".idea/" "*~" "*.iml" "local" ".envrc" ".direnv/" "*.swp" ];
       aliases = { fpush = "push --force-with-lease"; };
       package = pkgs.gitAndTools.gitFull;
+
+      ignores = [
+        "*~"
+        "*.swp"
+
+        "local" 
+
+        ".envrc" 
+        ".direnv/" 
+
+        ".idea/"
+        "*.iml"
+
+        "__pycache__"
+      ];
 
       difftastic.enable = true;
 
