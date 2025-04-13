52533b29f5327f2aef92449bd9ee5565fe4ff9ae
Janet Cobb <git@randomcat.org>
2025-04-13 18:57:11 -0400

    bear: add redirect for mail.unspecified.systems

--- hosts/bear/web.nix
+++ hosts/bear/web.nix
@@ -2,14 +2,26 @@
 
 {
   config = {
-    services.nginx.virtualHosts."unspecified.systems" = {
-      addSSL = true;
-      acmeRoot = config.security.acme.certs."unspecified.systems".webroot;
-      useACMEHost = "unspecified.systems";
+    services.nginx.virtualHosts = {
+      "unspecified.systems" = {
+        addSSL = true;
+        acmeRoot = config.security.acme.certs."unspecified.systems".webroot;
+        useACMEHost = "unspecified.systems";
 
-      serverAliases = [ "www.unspecified.systems" ];
+        serverAliases = [ "www.unspecified.systems" ];
 
-      locations."/".root = "${./home}";
+        locations."/".root = "${./home}";
+      };
+
+      "mail.unspecified.systems" = {
+        addSSL = true;
+        acmeRoot = config.security.acme.certs."unspecified.systems".webroot;
+        useACMEHost = "unspecified.systems";
+
+        locations."/" = {
+          return = "302 https://unspecified.systems";
+        };
+      };
     };
   };
 }
