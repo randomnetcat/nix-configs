1361423043c1cfca623614867bb7484f284a4e4c
Janet Cobb <git@randomcat.org>
2025-05-08 01:35:49 -0400

    shaw: listen for SSH on all interfaces

--- hosts/shaw/ssh.nix
+++ hosts/shaw/ssh.nix
@@ -1,20 +1,22 @@
 { config, lib, pkgs, ... }:
 
-let
-  birdsongIPs = let host = config.birdsong.hosts.shaw; in [ host.ipv4 host.ipv6 ];
-in
 {
   config = {
     services.openssh = {
       enable = true;
       openFirewall = false;
 
-      listenAddresses = map
-        (ip: {
-          addr = "[${ip}]";
+      listenAddresses = [
+        {
+          addr = "0.0.0.0";
           port = 22;
-        })
-        birdsongIPs;
+        }
+
+        {
+          addr = "[::]";
+          port = 22;
+        }
+      ];
 
       settings = {
         PermitRootLogin = "no";
@@ -24,11 +26,17 @@ in
       };
     };
 
-    networking.firewall.interfaces.wg-birdsong.allowedTCPPorts = [ 22 ];
+    networking.firewall.allowedTCPPorts = [ 22 ];
 
     systemd.services.sshd = {
       wants = [ "wireguard-wg-birdsong.service" ];
       after = [ "wireguard-wg-birdsong.service" ];
+    };
+
+    users.users.randomcat = {
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDHagOaeTR+/7FL9sErciMw30cmV/VW8HU7J3ZFU5nj9"
+      ];
     };
   };
 }
