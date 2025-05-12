ecea83ad96b029009e38518d9044b260814e8626
Janet Cobb <git@randomcat.org>
2025-05-12 19:33:12 +0100

    hosts/reese: fix plugin update script

--- hosts/reese/nomic-web/wiki/plugins/do-update-plugins.sh
+++ hosts/reese/nomic-web/wiki/plugins/do-update-plugins.sh
@@ -6,7 +6,7 @@ set -eu -o pipefail
 # loudly if it changes, and it seems unlikely to change very often.
 plugins_index="https://extdist.wmflabs.org/dist/extensions/"
 
-cd -- "$(dirname -- "$BASH_SOURCE")"
+cd -- "$(dirname -- "${BASH_SOURCE[@]}")"
 
 mw_raw_version="$(nix eval --raw .#nixosConfigurations.reese.config.services.mediawiki.package.version)"
 
@@ -50,7 +50,8 @@ ia_snapshot_to_raw() {
 }
 
 ia_preexisting_snapshot_url() {
-	declare snapshot_url="$(curl 'https://archive.org/wayback/available' -G --data "url=$plugin_url" | jq -r '.archived_snapshots.closest.url // empty')"
+	local snapshot_url
+	snapshot_url="$(curl 'https://archive.org/wayback/available' -G --data "url=$1" | jq -r '.archived_snapshots.closest.url // empty')"
 
 	# Print the result to stdout if non-empty; otherwise, output nothing.
 	if [[ -n "$snapshot_url" ]]; then
@@ -59,7 +60,8 @@ ia_preexisting_snapshot_url() {
 }
 
 ia_create_new_snapshot_url() {
-	declare snapshot_url="$(ia_snapshot_to_raw "$(savepagenow -c "$plugin_url")")"
+	local snapshot_url
+	snapshot_url="$(ia_snapshot_to_raw "$(savepagenow -c "$1")")"
 
 	# Print the result to stdout if non-empty; otherwise, output nothing.
 	if [[ -n "$snapshot_url" ]]; then
@@ -70,11 +72,16 @@ ia_create_new_snapshot_url() {
 while read -r plugin_name; do
 	echo "Plugin: $plugin_name" > /dev/stderr
 
-	plugin_url="$(cat -- "$work/plugin-urls" | grep -F -- "extensions/${plugin_name}-${mw_release}")"
+	plugin_url="$(cat -- "$work/plugin-urls" | grep -F -- "extensions/${plugin_name}-${mw_release}-")"
 
 	if [[ "$(printf '%s\n' "$plugin_url" | wc -l)" != "1" ]]; then
-		echo "Found unexpected plugin urls:" > /dev/stderr
-		printf '%s\n' "$plugin_url"
+		{
+			echo "Found unexpected plugin urls for plugin %s:" "$plugin_name"
+			printf '%s\n' "$plugin_url"
+			echo "Using first URL."
+		} > /dev/stderr
+
+		plugin_url="$(printf '%s\n' "$plugin_url" | head -n 1)"
 	fi
 
 	echo "Raw URL: $plugin_url" > /dev/stderr
@@ -84,11 +91,11 @@ while read -r plugin_name; do
 	# fall back to just using the original URL.
 
 	# Try using an existing snapshot, if one exists.
-	effective_url="$(ia_preexisting_snapshot_url)"
+	effective_url="$(ia_preexisting_snapshot_url "$plugin_url")"
 
 	# Otherwise, try to create a new snapshot and use that.
 	if [[ -z "$effective_url" ]]; then
-		effective_url="$(ia_create_new_snapshot_url)"
+		effective_url="$(ia_create_new_snapshot_url "$plugin_url")"
 	fi
 
 	# Otherwise, fall back to using the raw plugin URL.
