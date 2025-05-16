b197c887355808890763d7ba66fa77898f1b09cd
Janet Cobb <git@randomcat.org>
2025-05-16 22:34:53 +0100

    home: ensure VLC always uses JDK 17

--- home/wants/media-consumption.nix
+++ home/wants/media-consumption.nix
@@ -1,18 +1,38 @@
 { config, lib, pkgs, ... }:
 
+let
+  blurayJava = pkgs.jdk17;
+
+  rawVlc = pkgs.vlc.override {
+    libbluray = pkgs.libbluray.override {
+      jdk17 = blurayJava;
+      withAACS = true;
+      withBDplus = true;
+      withJava = true;
+    };
+  };
+
+  # libbluray breaks with Java that is too recent. (It appears that it pokes around in JDK
+  # internals that have since changed.) As a workaround, set JAVA_HOME when using VLC
+  # to a known-compatible JDK version.
+  wrappedVlc = rawVlc.overrideAttrs (oldAttrs: {
+    nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ pkgs.makeWrapper ];
+
+    postFixup = ''
+      ${oldAttrs.postFixup or ""}
+
+      for program in "$out"/bin/*; do
+        wrapProgram "$program" --set JAVA_HOME ${lib.escapeShellArg blurayJava.home}
+      done
+    '';
+  });
+in
 {
   home.packages = [
     pkgs.strawberry
     pkgs.makemkv
     pkgs.helvum
     pkgs.jellyfin-media-player
-
-    (pkgs.vlc.override {
-      libbluray = pkgs.libbluray.override {
-        withAACS = true;
-        withBDplus = true;
-        withJava = true;
-      };
-    })
+    wrappedVlc
   ];
 }
