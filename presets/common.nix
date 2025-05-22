f9b9ad17177d710115e720b1d8d87d3cdaca3fd2
Janet Cobb <git@randomcat.org>
2025-05-22 15:16:02 +0100

    presets/common: use default nixpkgs flake support

--- presets/common.nix
+++ presets/common.nix
@@ -13,16 +13,6 @@
   ];
 
   config = {
-    nix.registry.nixpkgs.to = {
-      type = "github";
-      owner = "NixOS";
-      repo = "nixpkgs";
-      rev = inputs.nixpkgs.rev;
-      narHash = inputs.nixpkgs.narHash;
-      lastModified = inputs.nixpkgs.lastModified;
-    };
-
-    nix.nixPath = [ "nixpkgs=flake:nixpkgs" ];
     nix.extraOptions = ''
       experimental-features = nix-command flakes
       accept-flake-config = false
@@ -38,12 +28,5 @@
         inherit inputs;
       };
     };
-
-    assertions = [
-      # {
-      #   assertion = inputs.nixpkgs.outPath == (toString pkgs.path);
-      #   message = "Nixpkgs path should be the same as nixpkgs input";
-      # }
-    ];
   };
 }
