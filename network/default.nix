ef69b14c47b03df098ed76cbb85663302cb96a4c
Janet Cobb <git@randomcat.org>
2025-05-22 01:54:07 +0100

    network: update Tailscale IPs

--- network/default.nix
+++ network/default.nix
@@ -11,33 +11,35 @@
       hosts = {
         bear = {
           hostKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIZ9Kn1CIcDHaleKHf7zO6O30Rbxs/FwL0/Ie+mEjZJr";
-          tailscaleIP4 = "100.85.165.130";
-          tailscaleIP6 = "fd7a:115c:a1e0:ab12:4843:cd96:6255:a582";
+          tailscaleIP4 = "100.91.170.206";
+          tailscaleIP6 = "fd7a:115c:a1e0:52a9:3704:2f18:e2ca:626a";
         };
 
         carter = {
           hostKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEHHHYG6A995Po05+JXQsvB79ZoIiSOJnW6AiJgVYPic";
-          tailscaleIP4 = "100.101.241.29";
-          tailscaleIP6 = "fd7a:115c:a1e0::101:f11f";
+          tailscaleIP4 = "100.78.202.172";
+          tailscaleIP6 = "fd7a:115c:a1e0:e7f4:1215:5998:f67:c202";
           isPortable = true;
         };
 
         leon = {
           hostKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJCuMBdSMXPBTj00Z4oRcFuo/BEzbVogwYsvixhkBuU9";
+
+          # Outdated: not set up with unspecified.systems.
           tailscaleIP4 = "100.94.148.11";
           tailscaleIP6 = "fd7a:115c:a1e0::d401:940b";
         };
 
         reese = {
           hostKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPd0qGxvcMLDwX1bqYpwOUL5c/CIgBllMFr+bGkwiwAn";
-          tailscaleIP4 = "100.90.31.23";
-          tailscaleIP6 = "fd7a:115c:a1e0:ab12:4843:cd96:625a:1f17";
+          tailscaleIP4 = "100.98.105.253";
+          tailscaleIP6 = "fd7a:115c:a1e0:1f6e:62c0:45c3:6b41:6403";
         };
 
         shaw = {
           hostKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMC0AomCZZiUV/BCpImiV4p/vGvFaz5QNc+fJLXmS5p";
-          tailscaleIP4 = "100.103.37.71";
-          tailscaleIP6 = "fd7a:115c:a1e0::f567:2547";
+          tailscaleIP4 = "100.109.173.202";
+          tailscaleIP6 = "fd7a:115c:a1e0:9be9:6c5a:2f1d:db18:4d1a";
         };
       };
 
