b5a145b1a1bbb8f135583c2db77ae2c3abebd1f4
Janet Cobb <git@randomcat.org>
2025-07-08 22:23:29 -0400

    carter: attempt to fix graphical artifacts

--- hosts/carter/graphical.nix
+++ hosts/carter/graphical.nix
@@ -9,6 +9,12 @@
     services.xserver.videoDrivers = [ "amd" ];
     boot.initrd.kernelModules = [ "amdgpu" ];
 
+    # Fix graphical artifacts
+    # https://bbs.archlinux.org/viewtopic.php?id=296990
+    boot.kernelParams = [
+      "amdgpu.dcdebugmask=0x10"
+    ];
+
     hardware.graphics = {
       enable = true;
       enable32Bit = true;
