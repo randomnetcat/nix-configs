0e0b09494c13620b4dfc397946e0ad7ee062f141
Janet Cobb <git@randomcat.org>
2025-07-12 00:15:28 -0400

    groves: user NetworkManager

--- hosts/groves/networking.nix
+++ hosts/groves/networking.nix
@@ -5,26 +5,16 @@
     ../../sys/impl/networking/wifi.nix
   ];
 
-  options = { };
-
   config = {
     # Let NetworkManager handle DHCP
     networking.useDHCP = false;
     networking.interfaces.enp46s0.useDHCP = false;
     networking.interfaces.wlp0s20f3.useDHCP = false;
 
-    environment.systemPackages = [
-      pkgs.networkmanagerapplet
-      pkgs.networkmanager-openvpn
-      pkgs.networkmanager-openconnect
-    ];
+    systemd.network.enable = false;
 
-    systemd.network = {
-      # Needed for birdsong wireguard.
+    networking.networkmanager = {
       enable = true;
-
-      # Internet connectivity is managed by NetworkManager.
-      wait-online.enable = false;
     };
   };
 }
