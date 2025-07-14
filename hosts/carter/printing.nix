129cb81b3bf7adc50f981515ec8513446a07c3a2
Janet Cobb <git@randomcat.org>
2025-07-14 11:54:11 -0400

    carter: disable herdware authentication for cups

--- hosts/carter/printing.nix
+++ hosts/carter/printing.nix
@@ -8,5 +8,14 @@
       pkgs.hplipWithPlugin
       pkgs.utsushi
     ];
+
+    # CUPS trying to read fingerprints causes it to freeze until a fingerprint
+    # is provided, even if a password is provided by HTTP Basic auth. So,
+    # disable it (and disable U2F authentication because it probably has a
+    # similar problem, though I haven't tested it).
+    security.pam.services.cups = {
+      fprintAuth = false;
+      u2fAuth = false;
+    };
   };
 }
