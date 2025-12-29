3a04c13d3e9874ec976ec6f1bae19ec86091b1f2
Janet Cobb <git@randomcat.org>
2025-12-29 20:03:08 +0000

    sys: fix NetworkManager wifi configuration

--- sys/impl/networking/wifi.nix
+++ sys/impl/networking/wifi.nix
@@ -7,7 +7,6 @@
   options = { };
 
   config = {
-    networking.wireless.enable = false; # Disable wpa_supplicant
     networking.networkmanager.enable = true; # Use NetworkManager for wifi
 
     users.users.randomcat.extraGroups = [ "networkmanager" ];
