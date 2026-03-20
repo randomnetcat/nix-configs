007a0233381279d97c49cc58b3523dccd427e5eb
Janet Cobb <git@randomcat.org>
2026-03-20 11:17:03 -0400

    sys: disable gnome-console

--- sys/impl/gnome-presets.nix
+++ sys/impl/gnome-presets.nix
@@ -3,5 +3,10 @@
 {
   config = lib.mkIf (config.services.desktopManager.gnome.enable) {
     services.gnome.gnome-keyring.enable = true;
+    programs.gnome-terminal.enable = true;
+
+    environment.gnome.excludePackages = [
+      pkgs.gnome-console
+    ];
   };
 }
