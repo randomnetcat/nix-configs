102bc0f3fbd13332453f48e19d5c4e8cae91c2cc
Janet Cobb <git@randomcat.org>
2026-04-02 00:39:42 -0400

    carter: add drivers for Epson printer

--- hosts/carter/printing.nix
+++ hosts/carter/printing.nix
@@ -7,7 +7,23 @@
     services.printing.drivers = [
       pkgs.hplipWithPlugin
       pkgs.utsushi
+      pkgs.epson-escpr
+      pkgs.epson-escpr2
+      pkgs.epsonscan2
     ];
+
+    # hardware.sane = {
+    #   enable = true;
+
+    #   extraBackends = [
+    #     pkgs.epsonscan2
+    #   ];
+
+    #   # Add apartment Epson printer.
+    #   netConf = ''
+    #     192.168.3.10
+    #   '';
+    # };
 
     # CUPS trying to read fingerprints causes it to freeze until a fingerprint
     # is provided, even if a password is provided by HTTP Basic auth. So,
